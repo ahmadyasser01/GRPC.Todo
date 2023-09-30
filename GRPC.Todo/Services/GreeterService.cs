a417ff451b8d8a67539bd15cfab0932ad763eb1f
ahmadyasser01 <ahmadyasser2814@gmail.com>
2023-10-01 00:04:41 +0300

    test

--- GRPC.Todo/Services/GreeterService.cs
+++ GRPC.Todo/Services/GreeterService.cs
@@ -15,7 +15,7 @@ namespace GRPC.Todo.Services
         {
             return Task.FromResult(new HelloReply
             {
-                Message = "Hello " + request.Name
+                Message = "Hello  " + request.Name
             });
         }
     }
